33169b68c7b0c5102b910c1086a48d464335ec2d
Michael Nizzari <michael.nizzari@gmail.com>
2017-04-13 13:52:58 -0400

    Removed redundant functions

--- grid-movement/scripts/facing_cursor.gd
+++ grid-movement/scripts/facing_cursor.gd
@@ -6,14 +6,14 @@ var cursor_pos
 
 func _ready():
 	player = get_parent()
-	tile_size = player.get_tile_size()
+	tile_size = player.tile_size
 	cursor_pos = {
 		"up":    Vector2( tile_size , -tile_size ),
 		"down":  Vector2( tile_size , tile_size*3 ),
 		"left":  Vector2( -tile_size , tile_size ),
 		"right": Vector2( tile_size*3 , tile_size )
 	}
-	set_cursor_direction( player.get_direction() )
+	set_cursor_direction( player.direction )
 
 func set_cursor_direction(dir):
 	if cursor_pos.has(dir):
